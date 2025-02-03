2c9dcac39b7b98eb9624c947ae59f31dffa211ed
Romain Herault <romain.herault@les-tilleuls.coop>
2025-02-03 18:58:08 +0100

    feat: add basic scene and wiggle

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Center, Environment, OrbitControls } from '@react-three/drei';
-import { Tamagotchi } from '@/components/Tamagotchi';
 import { Canvas } from '@react-three/fiber';
+import { Bedroom } from './components/Bedroom';
+import { Adult, Pet } from './components/Pet';
+import { PivotControls } from '@react-three/drei';
 
 const Experience = () => {
   return (
     <>
       <Canvas>
+        <pointLight position={[5, 5, 5]} />
         <Environment preset="sunset" />
-        <OrbitControls makeDefault />
-
+        <OrbitControls
+          makeDefault
+          maxDistance={6}
+          minDistance={4}
+          minAzimuthAngle={-Math.PI / 4}
+          maxAzimuthAngle={Math.PI / 1.5}
+          maxPolarAngle={Math.PI / 2}
+          enablePan={false}
+        />
         <Center>
-          <Tamagotchi />
+          <Bedroom scale={0.6} rotation-y={-Math.PI / 2} />
+          <PivotControls depthTest={false}>
+            <Pet scale={2} position={[-0.6, 0.1, 0]} />
+          </PivotControls>
         </Center>
       </Canvas>
     </>
---
+++ src/components/Pet.jsx
@@ -0,0 +1,60 @@
+import * as THREE from 'three';
+import React, { useRef } from 'react';
+import { useGLTF } from '@react-three/drei';
+import { useEffect } from 'react';
+import { WiggleBone } from 'wiggle';
+import { useFrame } from '@react-three/fiber';
+
+export function Pet(props) {
+  const { nodes, scene } = useGLTF('/assets/models/pet.glb');
+  const wiggleBones = useRef([]);
+
+  useEffect(() => {
+    wiggleBones.current.length = 0;
+    nodes.RootBone.traverse((bone) => {
+      if (bone.isBone && bone !== nodes.RootBone) {
+        const wiggleBone = new WiggleBone(bone, {
+          velocity: 0.2,
+        });
+
+        wiggleBones.current.push(wiggleBone);
+      }
+    });
+
+    return () => {
+      wiggleBones.current.forEach((wiggleBone) => {
+        wiggleBone.reset();
+        wiggleBone.dispose();
+      });
+    };
+  }, [nodes]);
+
+  useFrame(() => {
+    wiggleBones.current.forEach((wiggleBone) => {
+      wiggleBone.update();
+    });
+  });
+
+  return <primitive object={scene} scale={0.6} />;
+}
+
+useGLTF.preload('/assets/models/pet.glb');
+
+export function Adult(props) {
+  const { nodes } = useGLTF('/assets/models/pet-adulte.glb');
+  const body = new THREE.MeshStandardMaterial({ color: '#F9CB43' });
+  const clothes = new THREE.MeshStandardMaterial({ color: '#FFFFFF' });
+  return (
+    <group {...props} dispose={null}>
+      <group rotation={[-Math.PI / 2, 0, 0]}>
+        <group rotation={[Math.PI / 2, 0, 0]}>
+          <mesh castShadow receiveShadow geometry={nodes.Object_3.geometry} material={body} />
+          <mesh castShadow receiveShadow geometry={nodes.Object_4.geometry} material={clothes} />
+          <mesh castShadow receiveShadow geometry={nodes.Object_5.geometry} material={body} />
+        </group>
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload('/assets/models/pet-adulte.glb');
